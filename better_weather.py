4a81f72ecf600c52fe110905dffc9aef79915112
Madeline Scotti <scottimadeline@gmail.com>
2023-06-27 19:38:38 +0000

    broke up url more

--- better_weather.py
+++ better_weather.py
@@ -2,14 +2,17 @@ import requests
 import json
 
 key = "3c978d81b1e84cfc836183128232706"
-url_key = "https://api.weatherapi.com/v1/current.json?key=" + key
+url = "https://api.weatherapi.com/v1/"
+time = "current.json"
+url_key = url + time + "?key=" + key
 
 def weather_getter(zip):
     url =  url_key + "&q=" + zip
     response = requests.get(url)
     print(json.dumps(response.json(),indent=3))
     # add alerts
-    
+    # try to get date from data
+
 
 def main():
     zipcode = input("Enter your zipcode: ")
@@ -17,7 +20,7 @@ def main():
     # Print out current forecast and then ask if they want anymore data from future or past days
     # History has to be on or after Jan 1st 2010
     # Future has to be at leat 14 days into the future
-    time_range = input("How many days in the future (at least 14? ")
+    time_range = input("How many days in the future (at least 14)? ")
    
     url = "https://api.weatherapi.com/v1/future.json?key=" + key + "&q=" + zipcode + "&dt=2023-07-11"
 
