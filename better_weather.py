00cb1ba6f1a2fe261141fa9be3b1ba9573dacad3
Kalem Schrock <kalemschrock@gmail.com>
2023-06-27 17:50:51 +0000

    added ip address getter

--- better_weather.py
+++ better_weather.py
@@ -1,5 +1,14 @@
 import requests
 import json
+## importing socket module
+import socket
+## getting the hostname by socket.gethostname() method
+hostname = socket.gethostname()
+## getting the IP address using socket.gethostbyname() method
+ip_address = socket.gethostbyname(hostname)
+## printing the hostname and ip_address
+print(f"Hostname: {hostname}")
+print(f"IP Address: {ip_address}")
 
 url = "https://api.weather.gov/"
 ex_forecast = "points/40.678177,-73.944160"
