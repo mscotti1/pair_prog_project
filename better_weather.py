4687377bbac35944ccd7788e47a19efeb6d733e4
Madeline Scotti <scottimadeline@gmail.com>
2023-06-27 18:35:31 +0000

    back to the beginning

--- better_weather.py
+++ better_weather.py
@@ -1,14 +1,5 @@
 import requests
 import json
-## importing socket module
-import socket
-## getting the hostname by socket.gethostname() method
-hostname = socket.gethostname()
-## getting the IP address using socket.gethostbyname() method
-ip_address = socket.gethostbyname(hostname)
-## printing the hostname and ip_address
-print(f"Hostname: {hostname}")
-print(f"IP Address: {ip_address}")
 
 url = "https://api.weather.gov/"
 ex_forecast = "points/40.678177,-73.944160"
@@ -16,7 +7,8 @@ ex_forecast = "points/42.0345,-93.6203"
 
 ex_alerts = "alerts/active?area=KS"
 response = requests.get(url+ex_forecast)
+
 # response = requests.get(url+ex_alerts)
-print(response)
-print(json.dumps(response.json(), indent=3))
+# print(response)
+# print(json.dumps(response.json(), indent=3))
 
