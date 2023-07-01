e63409e773d77a2bdbd70e8f2374d902fe110e37
Kalem S <kalemschrock@gmail.com>
2023-07-01 14:22:27 -0500

    format again

--- test_whenever.py
+++ test_whenever.py
@@ -3,13 +3,11 @@ import datetime
 from whenever_methods import get_date_str
 
 
-
 class Test_whenever_weather(unittest.TestCase):
-
 
     def test_get_date_str(self):
         new_date = datetime.datetime(2023, 6, 5)
         self.assertEqual(get_date_str(new_date), "2023-06-05")
 
 
-#python3 -m unittest test_whenever.py
+# python3 -m unittest test_whenever.py
